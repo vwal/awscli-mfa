cf82c8b9f2d4429133cb05e37b14aaacbf117b6c
Ville Walveranta <ville@605.tv>
2018-03-18 16:40:53 -0500

    Added color codes, improved UX.

--- awscli-mfa.sh
+++ awscli-mfa.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# todo: AWS_SHARED_CREDENTIALS_FILE and AWS_CONFIG_FILE values should either
+#       be utilized, or their presence should not be allowed
+#       
+# todo: support profile names with spaces (AWS allows them)
+# 
+# todo: quote all 
+# 
+# todo: when there is only one single profile, bypass the profile 
+#       selection menu
+#       
+# todo: better documentation!
+
 DEBUG="false"
 # uncomment below to enable the debug output
 #DEBUG="true"
@@ -26,7 +38,83 @@ MFA_SESSION_LENGTH_IN_SECONDS=32400
 CONFFILE=~/.aws/config
 CREDFILE=~/.aws/credentials
 
-## FUNCTIONS
+# COLOR DEFINITIONS ==========================================================
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+
+
+# FUNCTIONS ==================================================================
 
 # `exists` for commands
 exists() {
@@ -346,9 +434,9 @@ fi
 ONEPROFILE="false"
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	[[ "$line" =~ ^\[(.*)\].* ]] &&
-		profile_ident=${BASH_REMATCH[1]}
+		profile_ident="${BASH_REMATCH[1]}"
 
-		if [ $profile_ident != "" ]; then
+		if [ "$profile_ident" != "" ]; then
 			ONEPROFILE="true"
 		fi 
 done < $CREDFILE
@@ -517,7 +605,6 @@ else
 		exit 1
 	else
 		echo
-		echo
 		echo "Executing this script as the AWS/IAM user \"$process_username\" (profile \"$currently_selected_profile_ident\")."
 	fi
 
@@ -536,7 +623,7 @@ else
 	declare -a mfa_mfasec
 	cred_profilecounter=0
 
-	echo -n "Please wait"
+	echo -ne "${BIWhite}Please wait"
 
 	# read the credentials file
 	while IFS='' read -r line || [[ -n "$line" ]]; do
@@ -679,27 +766,27 @@ else
 
 		fi
 	done < $CREDFILE
+	echo -e "${Color_Off}"
 
 	# create the profile selections
 	echo
-	echo
-	echo "AVAILABLE AWS PROFILES:"
+	echo -e "${Black}${On_White} AVAILABLE AWS PROFILES: ${Color_Off}"
 	echo
 	SELECTR=0
 	ITER=1
 	for i in "${cred_profiles[@]}"
 	do
 		if [ "${mfa_arns[$SELECTR]}" != "" ]; then
-			mfa_notify=", MFA configured"
+			mfa_notify="; ${Green}vMFAd enabled${Color_Off}"
 		else
-			mfa_notify="" 
+			mfa_notify="; vMFAd not configured" 
 		fi
 
-		echo "${ITER}: $i (${cred_profile_user[$SELECTR]}${mfa_notify})"
+		echo -en "${BIWhite}${ITER}: $i${Color_Off} (IAM: ${cred_profile_user[$SELECTR]}${mfa_notify})\n"
 
 		if [[ "${mfa_profile_status[$SELECTR]}" != "EXPIRED" &&
 			"${mfa_profile_status[$SELECTR]}" != "" ]]; then
-			echo "${ITER}m: $i MFA profile (${mfa_profile_status[$SELECTR]})"
+			echo -e "${BIWhite}${ITER}m: $i MFA profile${Color_Off} (${mfa_profile_status[$SELECTR]})"
 		fi
 
 		echo
@@ -714,8 +801,10 @@ else
 	mfaprofile="false"
 
 	# prompt for profile selection
-	printf "SELECT A PROFILE BY THE ID: "
+	printf "You can switch to a base profile to use it as-is, start an MFA session\nfor a profile if it it marked as \"vMFAd enabled\", or switch to an existing\nactive MFA session if any are available (indicated by the letter 'm' after\nthe profile ID, e.g. '1m'; NOTE: the expired MFA sessions are not shown).\n"
+	echo -en  "\n${BIWhite}SELECT A PROFILE BY THE ID: "
 	read -r selprofile
+	echo -en  "\n${Color_Off}"
 
 	# process the selection
 	if [[ "$selprofile" != "" ]]; then
@@ -798,21 +887,23 @@ else
 
 		# prompt for the MFA code
 		echo
-		echo -e "Enter the current MFA one time pass code for profile '${cred_profiles[$actual_selprofile]}' to start/renew the MFA session,"
+		echo -e "Enter the current MFA one time pass code for profile '${cred_profiles[$actual_selprofile]}' to start/renew an MFA session,"
 		echo "or leave empty (just press [ENTER]) to use the selected profile without the MFA."
 		
 		while :
 		do
+			echo -en "${BIWhite}"
 			read mfacode
+			echo -en "${Color_Off}"
 			if ! [[ "$mfacode" =~ ^$ || "$mfacode" =~ [0-9]{6} ]]; then
-				echo "The MFA code must be exactly six digits, or blank to bypass."
+				echo "The MFA pass code must be exactly six digits, or blank to bypass (to use the profile without an MFA session)."
 				continue
 			else
 				break
 			fi
 		done
 
-	elif [[ "$active_mfa" == "false" ]]; then   # no MFA configured (no MFA ARN); print a notice
+	elif [[ "$active_mfa" == "false" ]]; then   # no vMFAd configured (no vMFAd ARN); print a notice
 		
 		# this is used to determine whether to print MFA questions/details
 		mfaprofile="false"
@@ -820,7 +911,7 @@ else
 		# reset entered MFA code (just to be safe)
 		mfacode=""
 		echo
-		echo -e "MFA has not been set up for this profile."
+		echo -e "vMFAd has not been set up for this profile (run 'register-virtual-mfa-device.sh' script to configure the vMFAd)."
 	fi
 
 	if [[ "$mfacode" != "" ]]; then
