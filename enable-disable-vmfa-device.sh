67d0463d441a20e782d5085f5b37dbc70299ed37
Ville Walveranta <ville@605.tv>
2019-01-22 14:55:08 -0600

    Bumped version numbers; added license notice to enable-disable-vmfa-device.sh

--- enable-disable-vmfa-device.sh
+++ enable-disable-vmfa-device.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+################################################################################
+# version 2.1.0 - 22 January 2019 - MIT license
+# 
+# Copyright 2019 Ville Walveranta / 605 LLC
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
+
 # NOTE: Debugging mode prints the secrets on the screen!
 DEBUG="false"
 
